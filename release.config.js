96d0dcedd387f9e239a48d7a1314b66461ce302b
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2020-10-04 11:06:51 +0200

    chore: fix deployment

--- release.config.js
+++ release.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    "@semantic-release/git",
+    ["@semantic-release/git", {
+      assets: ['CHANGELOG.md'],
+      // eslint-disable-next-line no-template-curly-in-string
+      message: "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}"
+    }],
     "@semantic-release/npm",
     "@semantic-release/github",
   ],
